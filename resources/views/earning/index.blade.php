a7cdc28238f947082ef77ee64850e398730223dd
Kaiz Romero <keishner.romero@gmail.com>
2024-02-27 15:24:18 +1100

    fix bug

--- resources/views/earning/index.blade.php
+++ resources/views/earning/index.blade.php
@@ -243,7 +243,7 @@
 
         $.ajax({
             type: "PUT",
-            url: "expenses/" + id,
+            url: "earning/" + id,
             data: $('#editForm').serialize(),
             success: function(response) {
                 location.reload();
